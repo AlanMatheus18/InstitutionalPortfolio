fcd2449912d89bfa033fc70a5fbf53733c9027ac
alanmatheus <matheusalan334@gmail.com>
2025-06-20 00:04:36 -0300

    faceta01

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -1,60 +1,57 @@
-  import { ChevronDown } from "lucide-react";
-  import invisa from "../../src/assets/imgs/Banner05.webp";
+import { ChevronDown } from "lucide-react";
+import faceta from "../../src/assets/imgs/facetas.jpg";
 
-  export default function HeroSection() {
-    const scrollToForm = () => {
-      document
-        .getElementById("formulario")
-        ?.scrollIntoView({ behavior: "smooth" });
-    };
+export default function HeroSection() {
+  const scrollToForm = () => {
+    document
+      .getElementById("formulario")
+      ?.scrollIntoView({ behavior: "smooth" });
+  };
 
-    return (
-      <section
-        className="bg-[#290e49] bg-no-repeat bg-center bg-cover text-white min-h-screen flex flex-wrap md:flex-nowrap justify-center items-center relative overflow-hidden"
-        id="hero"
-        style={{ backgroundImage: `url(${invisa})` }}
-      >
-        <div className="bg-black bg-opacity-30 backdrop-blur-[5px] w-[100%] h-[100%] top-0 left-0 absolute"></div>
+  return (
+    <section
+      className="bg-[#290e49] bg-no-repeat bg-center bg-cover text-white min-h-screen flex flex-wrap md:flex-nowrap justify-center items-center relative overflow-hidden"
+      id="hero"
+      style={{ backgroundImage: `url(${faceta})` }}
+    >
+      <div className="bg-black bg-opacity-30 backdrop-blur-[5px] w-[100%] h-[100%] top-0 left-0 absolute"></div>
 
-        <div className="container mt-[20px] md:mt-[0px] md:mb-[-20px] px-20 relative z-10 w-2px">
-          <h1 className="text-4xl font-montserrat font-bold mb-6 leading-tight">
-            Implantes Dentários com Resultados{" "}
-            <span className="text-dental-secondary">Naturais</span> e{" "}
-            <span className="text-dental-secondary">Duradouros</span>
-          </h1>
-          <p className="text-xl md:text-xl mb-8 text-gray-100 max-w-2xl leading-relaxed">
-            Volte a sorrir com segurança e qualidade. Avaliação gratuita com
-            especialistas!
-          </p>
-          <div className="flex justify-center md:justify-start items-center">
-            <a
-              href="https://dentalsante.aiatende.dev.br/conversions/whatsapp/wbhk?text=Ol%C3%A1,%20vim%20pelo%C2%A0seu%C2%A0site"
-              target="_blank"
-              rel="noopener noreferrer"
+      <div className="container mt-[20px] md:mt-[0px] md:mb-[-20px] px-20 relative z-10 w-2px">
+        <h1 className="text-4xl font-montserrat font-bold mb-6 leading-tight">
+          Transforme Seu Sorriso com Facetas Dentárias de Alta Estética
+        </h1>
+        <p className="text-xl md:text-xl mb-8 text-gray-100 max-w-2xl leading-relaxed">
+          Descubra como alcançar dentes brancos, alinhados e com aparência natural em poucas sessões com nossos especialistas.
+        </p>
+        <div className="flex justify-center md:justify-start items-center">
+          <a
+            href="https://dentalsante.aiatende.dev.br/conversions/whatsapp/wbhk?text=Ol%C3%A1,%20vim%20pelo%C2%A0seu%C2%A0site"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              onClick={scrollToForm}
+              className="inline-block bg-[#0f8516] text-white w-[330px] font-montserrat font-bold py-4 px-8 rounded-full text-[15px] transition-all duration-300 hover:scale-105 shadow-xl"
             >
-              <button
-                onClick={scrollToForm}
-                className="inline-block bg-[#0f8516] text-white w-[330px] font-montserrat font-bold py-4 px-8 rounded-full text-[15px] transition-all duration-300 hover:scale-105 shadow-xl"
-              >
-                Agende uma Avaliação Gratuita
-              </button>
-            </a>
-          </div>
+              Agende uma Avaliação Gratuita
+            </button>
+          </a>
         </div>
-        <div className="md:w-[800px] h-[600px] scale-[0.90]" id="formulario">
-          <iframe
-            className="mt-[-80px] md:mt-[-80px]"
-            src="https://forms.kommo.com/rwvxtrv"
-            width={500}
-            height={650}
-            frameBorder="0"
-            scrolling="no"
-          ></iframe>
-        </div>
+      </div>
+      <div className="md:w-[800px] h-[600px] scale-[0.90]" id="formulario">
+        <iframe
+          className="mt-[-80px] md:mt-[-80px]"
+          src="https://forms.kommo.com/rwvxtrv"
+          width={500}
+          height={650}
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
+      </div>
 
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <ChevronDown className="text-dental-secondary text-2xl" />
-        </div>
-      </section>
-    );
-  }
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <ChevronDown className="text-dental-secondary text-2xl" />
+      </div>
+    </section>
+  );
+}
