317f71a0600fd454013a0d7e7c569f2c9c8e7e8a
Douglas Augusto <sunshinedn2003@gmail.com>
2025-06-24 18:41:22 -0300

    base /

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import NotFound from "@/pages/not-found";
 function Router() {
   return (
     <Switch>
-      <Route path="/lentes" component={Home} />
+      <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
