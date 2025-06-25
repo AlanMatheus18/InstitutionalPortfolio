92a10e20a441d004ffba66a4687021f3625b0113
alan matheus <matheusalan334@gmail.com>
2025-06-25 10:20:23 -0300

    sobre-position

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -43,8 +43,8 @@ export default function Home() {
       <MainSection />
       <ResultsSection />
       <TestimonialsVideo />
-      <StructureSection />
       <Sobre />
+      <StructureSection />
       <BenefitsSection/>
       <LocationsSection />
       <Footer />
